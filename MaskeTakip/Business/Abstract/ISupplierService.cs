d255d349b6cefa74527995c9b174cc4eaf90bb18
Sıla Tekbaş <sila.tekbas@atolyevizyon.com>
2024-02-26 17:42:55 +0300

    Tc Kimlik Bilgisi Sorgulama
    Engin Demiroğ'un Canlı - 5 Saatlik C# Programlama Kampı derindeki kodların dökümü.

---
+++ MaskeTakip/Business/Abstract/ISupplierService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ISupplierService
+    {
+        void GiveMask(Person person);
+
+    }
+}
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string ad = "Sıla";
+            //string soyad = "Tekbaş";
+            //int dogumYili = 2002;
+            //long tcNo = 11111111111;
+            //Vatandas vatandas1 = new Vatandas();
+            Person person1 = new Person();
+
+
+        }
+        static void SelamVer(string isim)
+        {
+            Console.WriteLine("Merhaba"+isim);
+        }
+
+        
+    }
+
+    //public class Vatandas
+    //{
+    //    public string  Ad { get; set; }
+    //    public string Soyad { get; set; }
+    //    public int Dogumyili { get; set; }
+    //    public long TcNo { get; set; }
+    //}
+}
